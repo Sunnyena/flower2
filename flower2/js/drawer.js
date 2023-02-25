57f297deb2a74ba3d76007fffe61a48f390cfd1c
ena <nishi07039en@icloud.com>
2023-02-25 15:14:32 +0900

    second commit

---
+++ flower2/js/drawer.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+
+$(".openbtn").click(function () {
+	$(this).toggleClass('active');
+    $("#g-nav").toggleClass('panelactive');
+});
+
+$("#g-nav a").click(function () {
+    $(".openbtn").removeClass('active');
+    $("#g-nav").removeClass('panelactive');
+});
